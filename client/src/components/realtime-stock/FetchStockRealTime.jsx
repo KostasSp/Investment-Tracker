c435b7d80d9545334949d366becdc6bc556be6c2
Kostas <kostasspanolios@gmail.com>
2022-05-20 09:25:15 +0100

    fix - push changes not added in previous commit, refactor real-time stock component

--- client/src/components/realtime-stock/FetchStockRealTime.jsx
+++ client/src/components/realtime-stock/FetchStockRealTime.jsx
@@ -1,44 +1,46 @@
-import { useState } from "react";
+import { useState, useRef, useEFfect } from "react";
 import { StockList } from "./RealTimeStockList";
 import "./App.scss";
+import { filterFetchedItem } from "../../utility/RealtimeFetchUtility";
+import { startCounter, resetCounter } from "../../utility/TimeCounterFunctions"; //useMemo these?
 
 export const FetchStock = () => {
   const key = process.env.REACT_APP_API_KEY;
-  const currentStock = ["AAPL", "COIN", "TSLA", "AMZN", "GOOGL"];
 
+  const API_CALL_LIMIT_PER_MINUTE = 4;
+
+  const apiCallCount = useRef(0);
+  const [limitReached, setLimitReached] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
   const [stockData, setStockData] = useState({ data: [] });
 
-  const addNewStock = (stock) => {
-    let newArray = stockData["data"];
-    newArray.push(stock);
-    setStockData({ data: newArray });
-  };
-
-  const deleteRow = (index) => {
-    let newArr = [...stockData["data"]];
-    newArr.splice(index, 1);
-    setStockData({ data: newArr });
-  };
-
-  //add a useRef to keep track of max 500 api calls daily, and max 5 at a time or per min
-  //also cache stocks already requested for that day, and bring it over instead of making API call everytime
-  //maybe if !currentFive.includes(stock), then Promise.all
-  const fetchStockData = () => {
-    Promise.all(
-      currentStock.map((stock) =>
-        fetch(
+  const fetchStockData = (current, index) => {
+    let stock;
+    if (current === "") return;
+    typeof index === "undefined"
+      ? (stock = current)
+      : (stock = stockData.data[index]["Meta Data"]["2. Symbol"]);
+    apiCallCount.current >= API_CALL_LIMIT_PER_MINUTE
+      ? resetCounter(setLimitReached, apiCallCount) //put the interval functions in a useEffect and clear it, otherwise it constantly run after the first API call
+      : fetch(
           `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=${key}`
         )
           .then((response) => response.json())
-          .then((data) => addNewStock(data))
-      )
-    );
+          .then((data) =>
+            filterFetchedItem(data, stockData.data, setStockData, "2. Symbol")
+          )
+          .then(apiCallCount.current++)
+          .catch((error) => {
+            !limitReached && setErrorMessage(true);
+            console.log(error);
+          });
+    startCounter(setLimitReached, apiCallCount);
   };
 
   return (
     <div>
       {/* {console.log(apiData)} */}
-      <StockList stockData={stockData} delete={deleteRow} />
+      <StockList stockData={stockData} />
       {/* centering failed because i was trying to center the button
        (which probably ended up centering IN the component), 
        instead of the whole component */}
