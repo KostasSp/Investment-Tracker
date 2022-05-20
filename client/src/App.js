0a61fc63a15703df053be6c2a100f87509fca599
Kostas <kostasspanolios@gmail.com>
2022-05-20 11:21:42 +0100

    add daily stock component route, backend API call

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { FetchStockRealTime } from "./components/realtime-stock/FetchStockRealTime.jsx";
 import SideBar from "./components/sidebar/SideBar.jsx";
 import { FetchCryptoRealTime } from "./components/realtime-crypto/FetchCryptoRealTime.jsx";
+import FetchStockDaily from "./components/daily-stock/FetchStockDaily";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,11 +11,14 @@ function App() {
       <BrowserRouter>
         <SideBar />
         <Routes>
-          <Route path={"/"} element={<FetchCryptoRealTime />} />
+          {["/", "/realtime-cryptocurrency-updates"].map((path, index) => (
+            <Route key={index} path={path} element={<FetchCryptoRealTime />} />
+          ))}
           <Route
             path="/realtime-stock-updates"
             element={<FetchStockRealTime />}
           />
+          <Route path="/daily-stock-updates" element={<FetchStockDaily />} />
         </Routes>
       </BrowserRouter>
     </div>
