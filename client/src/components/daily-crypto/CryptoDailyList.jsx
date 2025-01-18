fedce540e455e85cf52c63b01d7b469be78b0f73
KostasSp <kostasspanolios@gmail.com>
2025-01-18 09:40:17 +0000

    Refactor

--- client/src/components/daily-crypto/CryptoDailyList.jsx
+++ client/src/components/daily-crypto/CryptoDailyList.jsx
@@ -1,12 +1,34 @@
-import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import TrendingUpIcon from "@mui/icons-material/TrendingUp";
-import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import PropTypes from "prop-types";
+import {
+  ArrowDropUp,
+  ArrowDropDown,
+  DeleteOutline,
+  TrendingUp,
+  TrendingDown,
+} from "@mui/icons-material";
 import "../../App.scss";
 import { deleteRow, changeListOrder } from "../../utils/ListFunctions";
 
-export const CryptoDailyList = (props) => {
+export const CryptoDailyList = ({ cryptoData, resultsDisplayed, setCryptoData }) => {
+  if (!cryptoData?.data || cryptoData.data.length === 0) {
+    return <p>No data available</p>;
+  }
+
+  const displayedData = cryptoData.data.slice(0, resultsDisplayed);
+
+  const renderDailyChangeIcon = (priceChange) =>
+    priceChange > 0 ? (
+      <TrendingUp className="arrow-up" />
+    ) : (
+      <TrendingDown className="arrow-down" />
+    );
+
+  const handleDelete = (index) => deleteRow(index, cryptoData, setCryptoData);
+  const handleMoveUp = (index) =>
+    changeListOrder(index, index - 1, cryptoData, setCryptoData);
+  const handleMoveDown = (index) =>
+    changeListOrder(index, index + 1, cryptoData, setCryptoData);
+
   return (
     <div>
       <table className="styled-table">
@@ -20,68 +42,54 @@ export const CryptoDailyList = (props) => {
             <th>Daily change</th>
           </tr>
         </thead>
-
         <tbody>
-          {props.cryptoData.data.length > 0 &&
-            props.cryptoData.data
-              .slice(0, props.resultsDisplayed)
-              .map((data, index) => {
-                return (
-                  <tr key={index}>
-                    <td>
-                      <DeleteOutlineIcon
-                        className="action-icons"
-                        style={{ fontSize: "1.4em" }}
-                        onClick={() =>
-                          deleteRow(
-                            index,
-                            props.cryptoData,
-                            props.setCryptoData
-                          )
-                        }
-                      />
-                      <ArrowDropUpIcon
-                        className="action-icons"
-                        onClick={() =>
-                          changeListOrder(
-                            index,
-                            index - 1,
-                            props.cryptoData,
-                            props.setCryptoData
-                          )
-                        }
-                      />
-                      <ArrowDropDownIcon
-                        className="action-icons"
-                        onClick={() =>
-                          changeListOrder(
-                            index,
-                            index + 1,
-                            props.cryptoData,
-                            props.setCryptoData
-                          )
-                        }
-                      />
-                    </td>
-                    <td className="cryptolist-image">
-                      <img src={data["image"]} alt="icon"></img>
-                    </td>
-                    <td>{data["name"]}</td>
-                    <td>{data["last_updated"].slice(0, 10)}</td>
-                    <td>${data["current_price"].toFixed(2)}</td>
-                    <td className="daily-price-change">
-                      {data["price_change_percentage_24h"].toFixed(2)}%
-                      {data["price_change_24h"] > 0 ? (
-                        <TrendingUpIcon className="arrow-up" />
-                      ) : (
-                        <TrendingDownIcon className="arrow-down" />
-                      )}
-                    </td>
-                  </tr>
-                );
-              })}
+          {displayedData.map((data, index) => (
+            <tr key={data.id || index}>
+              <td>
+                <DeleteOutline
+                  className="action-icons"
+                  onClick={() => handleDelete(index)}
+                />
+                <ArrowDropUp
+                  className="action-icons"
+                  onClick={() => handleMoveUp(index)}
+                />
+                <ArrowDropDown
+                  className="action-icons"
+                  onClick={() => handleMoveDown(index)}
+                />
+              </td>
+              <td className="cryptolist-image">
+                <img src={data.image} alt="icon" />
+              </td>
+              <td>{data.name}</td>
+              <td>{data.last_updated.slice(0, 10)}</td>
+              <td>${data.current_price.toFixed(2)}</td>
+              <td className="daily-price-change">
+                {data.price_change_percentage_24h.toFixed(2)}%
+                {renderDailyChangeIcon(data.price_change_24h)}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
+};
+
+CryptoDailyList.propTypes = {
+  cryptoData: PropTypes.shape({
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        image: PropTypes.string,
+        name: PropTypes.string,
+        last_updated: PropTypes.string,
+        current_price: PropTypes.number,
+        price_change_percentage_24h: PropTypes.number,
+        price_change_24h: PropTypes.number,
+      })
+    ),
+  }),
+  resultsDisplayed: PropTypes.number,
+  setCryptoData: PropTypes.func.isRequired,
 };
